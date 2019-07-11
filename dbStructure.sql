9b4b7d00a6bf56944db01653a07aadd1012cdc6d
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-11 19:38:09 -0400

    working....

--- dbStructure.sql
+++ dbStructure.sql
@@ -1,6 +1,7 @@
 
 
-CREATE TABLE IF NOT EXISTS student (
+DROP TABLE IF EXISTS student;
+CREATE TABLE student (
     StudentId INT AUTO_INCREMENT NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
@@ -9,8 +10,8 @@ CREATE TABLE IF NOT EXISTS student (
     PRIMARY KEY (StudentId)
 )  ENGINE=INNODB;
 
-
-CREATE TABLE IF NOT EXISTS Advisor (
+DROP TABLE IF EXISTS advisor;
+CREATE TABLE advisor (
 	EmployeeId INT NOT NULL,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
@@ -22,6 +23,20 @@ CREATE TABLE IF NOT EXISTS Advisor (
 
 -- Between Student and Advisor , We either create a new table which will be a weak relation
 -- or create a field 'AssignedAdvisor' in Student Table which will link student to Advisor/SuperVisor
- 
- 
 -- CREATE TABLE IF NOT EXISTS AssignedAdvisor ();
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE  department (
+	Name CHAR(60) NOT NULL,
+    -- ChairPerson can be null if no one is assigned yet
+    ChairPerson CHAR(60),
+	Building VARCHAR(30) NOT NULL
+)  ENGINE=INNODB;
+
+
+DROP TABLE IF EXISTS program;
+CREATE TABLE program (
+	Name CHAR(60) NOT NULL,
+	TypeOfProgram VARCHAR(30) NOT NULL
+    -- add more
+)  ENGINE=INNODB;
