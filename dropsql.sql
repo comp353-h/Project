c415c4a12f3c7d6c5b92cc569b00038cb95fa181
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-14 13:09:38 -0400

    Add files via upload

---
+++ dropsql.sql
@@ -0,0 +1,20 @@
+-- SELECT concat(DROP TABLE IF EXISTS `, table_name, `;)
+-- FROM information_schema.tables
+-- WHERE table_schema = hrc353_1;
+
+
+
+-- adjusted order of dropping for foreign key constraints
+DROP TABLE IF EXISTS `StudentAdvisor`;
+DROP TABLE IF EXISTS `Advisor`;
+DROP TABLE IF EXISTS `Class`;
+DROP TABLE IF EXISTS `StudentProgram`;
+DROP TABLE IF EXISTS `Course`;
+DROP TABLE IF EXISTS `Program`;
+DROP TABLE IF EXISTS `Department`;
+DROP TABLE IF EXISTS `Instructor`;
+DROP TABLE IF EXISTS `ResearchFunding`;
+DROP TABLE IF EXISTS `Section`;
+DROP TABLE IF EXISTS `Student`;
+DROP TABLE IF EXISTS `Supervisor`;
+DROP TABLE IF EXISTS `TeachingAssistant`;
