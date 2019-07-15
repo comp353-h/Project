6e78d6f20d08bd9ab2738d0891fcd390e30b9e19
Wmbat <wmbat@protonmail.com>
2019-07-15 12:48:06 -0400

    Quick Fix Program and Courses tables

--- populate.sql
+++ populate.sql
@@ -11,42 +11,26 @@ INSERT INTO Department VALUES
 ( 10, "Aerospace Engineering" );
 
 INSERT INTO Program VALUES
-( 1, 2, `Building Engineering`, `Undergraduate`, 90.0 ),
-( 2, 2, `Building Engineering`, `Graduate`, 44.0 ),
-( 3, 2, `Civil Engineering`, `Undergraduate`, 90.0 ),
-( 4, 2, `Civil Engineering`, `Graduate`, 44.0 ),
-( 5, 2, `Environmental Engineering`, `Graduate`, 44.0 ),
-( 6, 1, `Computer Science`, `Undergraduate`, 90.0 ),
-( 7, 1, `Computer Science`, `Graduate`, 44.0 ),
-( 8, 1, `Software Engineering`, `Undergraduate`, 90.0 ),
-( 9, 3, `Chemical Engineering`, `Graduate`, 44.0 ),
-( 10, 4, `Computer Engineering`, `Undergraduate`, 90.0 ),
-( 11, 4, `Electrical Engineering`, `Undergraduate`, 90.0 ),
-( 12, 4, `Electrical & Computer Engineering`, `Graduate`, 44.0 ),
-( 13, 5, `Aerospace Engineering`, `Undergraduate`, 90.0 ),
-( 14, 5, `Aerospace Engineering`, `Graduate`, 44.0 ),
-( 15, 5, `Industrial Engineering`, `Undergraduate`, 90.0 ),
-( 16, 5, `Industrial Engineering`, `Graduate`, 44.0 ),
-( 17, 5, `Mechanical Engineering`, `Undergraduate`, 90.0 ),
-( 18, 5, `Mechanical Engineering`, `Graduate`, 44.0 );
+( 1, 1, "Computer Application", "Undergraduate", 90.0 ),
+( 2, 1, "Computer Games", "Undergraduate", 90.0 ),
+( 3, 1, "Information Systems", "Undergraduate", 90.0 ),
+( 4, 1, "Computer Systems", "Undergraduate", 90.0 ),
+( 5, 1, "Web services & Applications", "Undergraduate", 90.0 );
 
 INSERT INTO Course VALUES
-( `MATH201`, `ELementary Functions`, `6`, `3.00` ), 
-( `MATH202`, `College Algebra`, `6`, `3.00`, `MATH201` ),
-( `MATH203`, `Differential & Integral Calculus I`, `6`, `3.00` ),
-( `MATH204`, `Vectors and Matrices`, `6`, `3.00` ),
-( `MATH205`, `Differential & Integral Calculus II`, `6`, `3.00` ),
-( `BCEE231`, `Structured Programming and Applications for Building and Civil Engineers`, `2`, `3.00`, `MATH204` ),
-( `BLDG212`, `Building Engineering Drawing and Introduction to Design`, `2`, `3.00` ),
-( `BCEE371`, `Surveying`, `2`, `3.00`, `BCEE231` ),
-( `CIVI212`, `Civil Engineering Drawing and Introduction to Design`, `2`, `3.00` ),
-( `CIVI231`, `Geology for Civil Engineers`, `2`, `3.00` ),
-( `COMP232`, `Mathematics for Computer`, `1`, `3.00`, `MATH204` ),
-( `COMP233`, `Probability and Statistics for Computer Science`, `1`, `3.00`, `MATH205` ),
-( `COMP248`, `Object-Oriented Programming`, `1`, `3.50`, `MATH204` ),
-( `COMP249`, `Object-Oriented Programming`, `1`, `3.50`, `MATH205` ),
-( `COMP353`, `Databases`, `1`, `4.00`, `COMP232` ),
-( `SOEN228`, `System Hardware`, `1`, `3.00`, `MATH204` );
-
-INSERT INTO `Student` VALUES
-( 
+( "MATH201", "ELementary Functions", 5, 3.00 ), 
+( "MATH202", "College Algebra", 5, 3.00, "MATH201" ),
+( "MATH203", "Differential & Integral Calculus I", 5, 3.00 ),
+( "MATH204", "Vectors and Matrices", 5, 3.00 ),
+( "MATH205", "Differential & Integral Calculus II", 5, 3.00 ),
+( "BCEE231", "Structured Programming and Applications for Building and Civil Engineers", 3, 3.00, "MATH204" ),
+( "BLDG212", "Building Engineering Drawing and Introduction to Design", 3, 3.00 ),
+( "BCEE371", "Surveying", 3, 3.00, "BCEE231" ),
+( "CIVI212", "Civil Engineering Drawing and Introduction to Design", 4, 3.00 ),
+( "CIVI231", "Geology for Civil Engineers", 4, 3.00 ),
+( "COMP232", "Mathematics for Computer", 1, 3.00, "MATH204" ),
+( "COMP233", "Probability and Statistics for Computer Science", 1, 3.00, "MATH205" ),
+( "COMP248", "Object-Oriented Programming", 1, 3.50, "MATH204" ),
+( "COMP249", "Object-Oriented Programming", 1, 3.50, "MATH205" ),
+( "COMP353", "Databases", 1, 4.00, "COMP232" ),
+( "SOEN228", "System Hardware", 1, 3.00, "MATH204" );
