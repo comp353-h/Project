bedddbb4ddac7125a95aef3607fb795e71ad8d92
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-16 18:59:58 -0400

    papi

--- trigger.sql
+++ trigger.sql
@@ -1,11 +1,12 @@
 
 DROP TRIGGER IF EXISTS check_hours_insert_trigger;
+
 DELIMITER $$
 CREATE TRIGGER check_hours_insert_trigger BEFORE INSERT ON TeachingAssistantRoles
 FOR EACH ROW
 BEGIN
 DECLARE totalhours INT;
-SET totalhours = (SELECT SUM(hours) FROM hrc353_1.TeachingAssistantRoles WHERE teachingAssistantID=NEW.teachingAssistantID);
+SET totalhours = (SELECT SUM(hours) FROM TeachingAssistantRoles WHERE teachingAssistantID=NEW.teachingAssistantID);
 if ( totalhours ) > 260 THEN
 	SIGNAL SQLSTATE '45000'
            SET MESSAGE_TEXT = 'check constraint on hours failed';
@@ -14,12 +15,13 @@ END;$$
 
 
 DROP TRIGGER IF EXISTS check_hours_update_trigger;
+
 DELIMITER $$
 CREATE TRIGGER check_hours_update_trigger BEFORE UPDATE ON TeachingAssistantRoles
 FOR EACH ROW
 BEGIN
 DECLARE totalhours INT;
-SET totalhours = (SELECT SUM(hours) FROM hrc353_1.TeachingAssistantRoles WHERE teachingAssistantID=NEW.teachingAssistantID);
+SET totalhours = (SELECT SUM(hours) FROM TeachingAssistantRoles WHERE teachingAssistantID=NEW.teachingAssistantID);
 if ( totalhours ) > 260 THEN
 	SIGNAL SQLSTATE '45000'
            SET MESSAGE_TEXT = 'check constraint on hours failed';
