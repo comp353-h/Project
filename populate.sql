46cfdadb1f47403fae50dfa50642a5f5e86c0f6a
Wmbat <wmbat@protonmail.com>
2019-07-16 15:27:16 -0400

    Teacher stuff

--- populate.sql
+++ populate.sql
@@ -82,20 +82,22 @@ INSERT INTO Section VALUES
 ( "DC", "COMP352", 4, 411, "H", "12:45:00", "14:00:00" ),
 ( "AA", "COMP352", 3, 907, "H", "14:30:00", "15:45:00" ),
 ( "WW", "COMP352", 1, 435, "H", "14:30:00", "15:45:00" ),
-( "AA", "COMP248", 1, 405, "H", "14:00:00", "15:15:00" ),
-( "CA", "COMP233", 1, 409, "H", "09:00:00", "10:15:00" ),
-( "BB", "COMP248", 2, 407, "H", "14:00:00", "15:15:00" ),
+( "AA", "COMP248", 6, 405, "H", "14:00:00", "15:15:00" ),
+( "CA", "COMP233", 6, 409, "H", "09:00:00", "10:15:00" ),
+( "RR", "COMP232", 6, 406, "H", "14:45:00", "17:30:00" ),
+( "BB", "COMP248", 7, 407, "H", "14:00:00", "15:15:00" ),
 ( "BB", "MATH204", 2, 535, "H", "09:00:00", "10:15:00" ),
 ( "CC", "COMP353", 2, 1123, "JM", "14:45:00", "17:30:00" );
 
 INSERT INTO InstructorHistory VALUES
 -- instructorID, termID, courseID, sectionID.
 ( 3, 1, "COMP352", "WW" ),
-( 3, 4, "COMP352", "BA" ),
 ( 2, 3, "COMP352", "AA" ),
+( 3, 4, "COMP352", "BA" ),
 ( 1, 4, "COMP352", "DC" ),
-( 1, 1, "COMP248", "AA" ),
-( 2, 2, "COMP248", "BB" );
+( 1, 6, "COMP248", "AA" ),
+( 2, 6, "COMP233", "CA" ),
+( 2, 7, "COMP248", "BB" );
 
 INSERT INTO Student VALUES
 -- studentID, firstName, lastName, phone, email, dateOfBirth, studentType, gpa.
@@ -117,8 +119,9 @@ INSERT INTO StudentProgram VALUES
 INSERT INTO StudentCourses VALUES
 -- studentID, courseID,	grade, termID, sectionID.
 ( 1, "COMP353", "CC", 2, "A+" ),
-( 1, "COMP248", "AA", 1, "A" ),
+( 1, "COMP248", "AA", 6, "A" ),
 ( 2, "COMP353", "CC", 2, "B" ),
-( 2, "COMP233", "CA", 1, "C-" ),
+( 2, "COMP233", "CA", 6, "C-" ),
+( 3, "COMP233", "CA", 6, "F" ),
 ( 3, "COMP353", "CC", 2, "A+" ),
 ( 4, "MATH204", "BB", 2, "C" );
