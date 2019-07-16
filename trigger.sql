2802ab2ca93c285f514786aec4a247aafe62267b
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-16 19:10:11 -0400

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,29 +1,26 @@
-
 DROP TRIGGER IF EXISTS check_hours_insert_trigger;
-
 DELIMITER $$
 CREATE TRIGGER check_hours_insert_trigger BEFORE INSERT ON TeachingAssistantRoles
 FOR EACH ROW
 BEGIN
 DECLARE totalhours INT;
 SET totalhours = (SELECT SUM(hours) FROM TeachingAssistantRoles WHERE teachingAssistantID=NEW.teachingAssistantID);
-if ( totalhours ) > 260 THEN
+IF ( totalhours ) > 260 THEN
 	SIGNAL SQLSTATE '45000'
-           SET MESSAGE_TEXT = 'check constraint on hours failed';
+           SET MESSAGE_TEXT = 'Insert will make the TA work for more 260 hours';
     END IF;
 END;$$
 
 
 DROP TRIGGER IF EXISTS check_hours_update_trigger;
-
 DELIMITER $$
 CREATE TRIGGER check_hours_update_trigger BEFORE UPDATE ON TeachingAssistantRoles
 FOR EACH ROW
 BEGIN
 DECLARE totalhours INT;
 SET totalhours = (SELECT SUM(hours) FROM TeachingAssistantRoles WHERE teachingAssistantID=NEW.teachingAssistantID);
-if ( totalhours ) > 260 THEN
+IF ( totalhours ) > 260 THEN
 	SIGNAL SQLSTATE '45000'
-           SET MESSAGE_TEXT = 'check constraint on hours failed';
+           SET MESSAGE_TEXT = 'Update will make the TA work for more 260 hours';
     END IF;
 END;$$
